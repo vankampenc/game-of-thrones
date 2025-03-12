62454efdfe176e54f9ec8cc80fe66070fc28c6ce
Christopher Van Kampen <christopheryungvankampen@gmail.com>
2025-03-12 12:23:55 -0400

    pagination

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 This is a side project I am working on to further solidify the skills I am learning in React. Please note that this is still being developed and not a reflection of the final iteration.
 
 ## Viewing Instructions
-To view, please run "npm start" and view in Vite.
+To view, please run "npm run dev" and view in Vite.
--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,26 @@ function App() {
 
   const [houses, setHouses] = useState([])
 
+  const [page, setPage] = useState(1)
+
+  const itemsPerPage = 50
+
+  const onNextPageClick = () => {
+    setPage(page+1)
+  } 
+  const onPreviousPageClick = () => {
+    setPage(page-1)
+  }
+
   useEffect(() => {
-    fetch("https://www.anapioficeandfire.com/api/houses?page=1&pageSize=50&hasDiedOut=false").then((res) => {
+    fetch(`https://www.anapioficeandfire.com/api/houses?page=${page}&pageSize=${itemsPerPage}&hasDiedOut=false`).then((res) => {
       return res.json()
     })
       .then((data) => {
         console.log("data", data)
         setHouses(data)
       })
-  }, [])
+  }, [page])
 
   //components
   return (
@@ -25,6 +36,14 @@ function App() {
           houses.map(house => <House key={house.url} house={house} />)
         }
       </div>
+      <button 
+        disabled={page <=1 ? true : false}
+        onClick={onPreviousPageClick}>
+          Previous</button>
+      <button 
+        disabled={houses.length < 50}
+        onClick={onNextPageClick}>
+          Next</button>
     </>
   )
 }
